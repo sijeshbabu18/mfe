cc825e86e99d433c774366a095a45323dbd2bb7a
Sijesh <skariyil@thoughtlinedigital.com>
2026-07-12 00:41:56 +0530

    added modules

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,21 +1,26 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
-  mode: 'development',
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:3000/",
+  },
   devServer: {
-    port: 8080,
+    port: 3000,
     historyApiFallback: {
-      index: 'index.html',
+      historyApiFallback: true,
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
-        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+        auth: "auth@http://localhost:8082/remoteEntry.js",
+        dashboard: "dashboard@http://localhost:8083/remoteEntry.js",
       },
       shared: packageJson.dependencies,
     }),
